5d735c3139d174d861468c292314090d94dc36c7
rsolab7 <rsolab7@gmail.com>
2017-09-27 14:22:23 -0400

    Added RecycleView template for metroStations Added RecycleView template for Landmarks

--- app/src/main/java/com/gwmobile/metroexplorer/LandmarksAdapter.kt
+++ app/src/main/java/com/gwmobile/metroexplorer/LandmarksAdapter.kt
@@ -1,9 +1,7 @@
 package com.gwmobile.metroexplorer
 
+
 import android.content.Context
-import android.graphics.BitmapFactory
-import android.support.v4.content.ContextCompat
-import android.support.v7.graphics.*
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
@@ -11,9 +9,8 @@ import android.view.ViewGroup
 import kotlinx.android.synthetic.main.row_places.view.*
 import com.squareup.picasso.Picasso
 
-class MetroStationAdapter(private var context: Context):
-
-        RecyclerView.Adapter<MetroStationAdapter.ViewHolder>(){
+class LandmarksAdapter(private var context: Context):
+        RecyclerView.Adapter<LandmarksAdapter.ViewHolder>(){
     override fun getItemCount() = PlaceData.placeList().size
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
--- app/src/main/java/com/gwmobile/metroexplorer/PlaceData.kt
+++ app/src/main/java/com/gwmobile/metroexplorer/PlaceData.kt
@@ -1,13 +1,11 @@
 package com.gwmobile.metroexplorer
-
-/**
- * Created by Roberto on 9/27/17.
- */
 import java.util.ArrayList
 
 object PlaceData {
 
     var placeNameArray = arrayOf("Bora Bora", "Canada", "Dubai", "Hong Kong", "Iceland", "India", "Kenya", "London", "Switzerland", "Sydney")
+    var MetroArray = arrayOf("Addison Road-Seat Pleasant","Anacostia","Archives-Navy Memorial-Penn Quarter ","Arlington Cementary", "Ballston", "Bennig Road", "bethesda","braddockRoad"
+            ,"brach Ave","Brookland-CUA", "Capitol Heights", "Capitol South","Cheverly","Clarendon","Cleveland Park","Dupont Cirlce")
 
     fun placeList(): ArrayList<Place> {
         val list = ArrayList<Place>()
