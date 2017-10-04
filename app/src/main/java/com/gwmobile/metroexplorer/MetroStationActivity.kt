17e1c48e7e4d8ffb254b24af9894ef5151d4c509
rsolab7 <rsolab7@gmail.com>
2017-10-04 12:26:43 -0400

    Added MenuBar for MetroStation and Landmark activity.  Mentro stations has a search option  Landmark Favorite  -- functionality still to be implemented.
     Deleted unused images

--- app/src/main/java/com/gwmobile/metroexplorer/MetroStationActivity.kt
+++ app/src/main/java/com/gwmobile/metroexplorer/MetroStationActivity.kt
@@ -8,6 +8,7 @@ import android.support.v4.app.ActivityOptionsCompat
 import android.support.v4.util.Pair
 
 import android.support.v7.widget.LinearLayoutManager
+import android.view.Menu
 import android.view.View
 import android.view.Window
 import android.widget.ImageView
@@ -21,8 +22,15 @@ class MetroStationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_metro_station)
+        setSupportActionBar(game_toolbar)
+
         val recyclerView = rv_station_list
         recyclerView.layoutManager = LinearLayoutManager(this, LinearLayout.VERTICAL, false)
         FetchMetroStationsManager.getMetroStationList(this, recyclerView)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.metro, menu)
+        return true
     }
 }
