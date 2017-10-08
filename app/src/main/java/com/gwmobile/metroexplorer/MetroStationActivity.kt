3b066b91edadcef2c58f82d675f2a9b544e2802c
rsolab7 <rsolab7@gmail.com>
2017-10-08 13:33:07 -0400

    AppBar updates

--- app/src/main/java/com/gwmobile/metroexplorer/MetroStationActivity.kt
+++ app/src/main/java/com/gwmobile/metroexplorer/MetroStationActivity.kt
@@ -8,8 +8,20 @@ import android.widget.LinearLayout
 import com.gwmobile.metroexplorer.manager.FetchMetroStationsManager
 import kotlinx.android.synthetic.main.activity_menu.*
 import kotlinx.android.synthetic.main.activity_metro_station.*
+import android.support.v4.widget.SearchViewCompat.setSearchableInfo
+import android.support.v4.view.MenuItemCompat.getActionView
+import android.content.Context.SEARCH_SERVICE
+import android.app.SearchManager
+import android.content.Context
+import android.view.MenuItem
+import android.widget.SearchView
+import android.widget.SearchView.OnQueryTextListener
+import com.gwmobile.metroexplorer.R.id.game_toolbar
+import com.gwmobile.metroexplorer.R.id.rv_station_list
+import org.jetbrains.anko.toast
 
-class MetroStationActivity : AppCompatActivity() {
+
+class MetroStationActivity : AppCompatActivity()  {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,6 +36,16 @@ class MetroStationActivity : AppCompatActivity() {
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.metro, menu)
+        // Associate searchable configuration with the SearchView
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchView = menu!!.findItem(R.id.search).actionView as SearchView
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(componentName))
+//        var queryMetro = SearchManager.QUERY
+//        toast(queryMetro)
         return true
     }
+
+
+
 }
---
+++ app/src/main/java/com/gwmobile/metroexplorer/model/UserLocation.kt
@@ -0,0 +1,6 @@
+package com.gwmobile.metroexplorer.model
+
+/**
+ * Created by Roberto on 10/4/17.
+ */
+data class UserLocation(var latitude: String, var longitude: String)
